250d0244276a17ca987611b2a5fb30a271eb4718
denniyqugan <maddenniy@gmail.com>
2021-10-20 23:20:56 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+  });
